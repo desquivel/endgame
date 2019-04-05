e29e209d01f531033f0082344889e556c55b203e
Daniel Esquivel Herrera <desquivel@syesoftware.com>
2019-04-05 17:33:55 -0600

    Código base para el primer despliegue

---
+++ captura-de-linea/src/main/java/com/endgame/capturadelinea/CapturaDeLineaApplication.java
@@ -0,0 +1,13 @@
+package com.endgame.capturadelinea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapturaDeLineaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapturaDeLineaApplication.class, args);
+    }
+
+}
