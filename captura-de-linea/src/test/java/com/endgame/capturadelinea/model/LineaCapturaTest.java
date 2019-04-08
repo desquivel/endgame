e04da0eb6c15c2e56b85e427e2a8f5c02b96cd89
jrivera <jrivera@syesoftware.com>
2019-04-08 15:43:09 -0500

    Test LineaCaptura

--- captura-de-linea/src/test/java/com/endgame/capturadelinea/model/LineaCapturaTest.java
+++ captura-de-linea/src/test/java/com/endgame/capturadelinea/model/LineaCapturaTest.java
@@ -1,13 +1,17 @@
-package  com.endgame.capturalinea.model;
-import org.junit.*;
+package com.endgame.capturadelinea.model;
+
+import com.endgame.capturadelinea.model.LineaCaptura;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class LineaCapturaTest{
 
-    public testLineaCaptura(){
+    public void testLineaCaptura(){
         LineaCaptura ln = new LineaCaptura();
 
         assertNotNull(ln);
-        asserEquals("qwertyui123456789xc",ln.crear());
+        assertEquals("qwertyui123456789xc",ln.generar());
 
     }
 
